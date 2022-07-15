5d08f2255c1e595ec070da540f5036406a98e226
Hector Diaz | U-Payments <hector.diaz@u-payments.com>
2022-07-15 16:51:20 -0400

    applying inheritance with interfaces

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,15 @@
 import { addProduct } from "./products/product.service";
 
 addProduct({
-  id: '1',
+  id: 1,
   title: 'P1',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock:90,
   category:{
     id:1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
     name: 'C1'
   }
 });
