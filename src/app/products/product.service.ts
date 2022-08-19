ab24be1a8543a373850f036bf8c934fbad60613b
Hector Diaz | U-Payments <hector.diaz@u-payments.com>
2022-08-19 11:06:12 -0400

    add new ts data property --readonly--

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Sizes, Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) =>{
+  // data.id = "a3b435c4d"; --> Cannot assign to 'id' because it is a read-only property.
   products.push(data);
 };
